26ab102532db4cd4c77af8e3ca224748cae3295c
galder <lemmyg@gmail.com>
2023-10-22 09:35:40 +0100

    added fuction

--- dictTest/src/main.rs
+++ dictTest/src/main.rs
@@ -64,6 +64,44 @@ impl Config {
     }
 }
 
+fn initialize_layers() -> Vec<Button>{
+
+    let config_path = "/home/galder/git/rust/dictTest/etc/tiny-dfr.conf";
+    let config = Config::from_file(config_path).unwrap();
+    let key_map = HashMap::from([
+        ("Esc", Key::Esc),
+        ("F1", Key::F1),
+        ("F2", Key::F2),
+        ("F3", Key::F3),
+        ("F4", Key::F4),
+        ("F5", Key::F5),
+        ("F6", Key::F6),
+        ("F7", Key::F7),
+        ("F8", Key::F8),
+        ("F9", Key::F9),
+        ("F10", Key::F10),
+        ("F11", Key::F11),
+        ("F12", Key::F12),
+    ]);
+
+    println!("Creating a vector from the config");
+    let mut primary_layer_buttons = Vec::new();
+    for button_config in &config.layers.primary_layer_buttons.buttons {
+        let label = button_config.label.as_str();
+        let key = &button_config.key.as_str();
+        match key_map.get(key) {
+            Some(value) => {
+                primary_layer_buttons.push(Button::new_text(label, *value));
+                println!("Found key {}: with value {:?}", label, value );
+            }
+            None => println!("Could not find {key} in the map.")
+        }
+    }
+    
+    return primary_layer_buttons
+
+}
+
 fn main() {
 
     let config_path = "/home/galder/git/rust/dictTest/etc/tiny-dfr.conf";
@@ -104,24 +142,11 @@ fn main() {
     
     for (key, value) in &key_map {
         println!("iterate: key {}: with value {:?}", key, value );
-    let mut primary_layer_buttons = Vec::new();
-    for (key, value) in KEY_MAP.iter() {
-        primary_layer_buttons.push(Button::new_text(key, *value));
-        println!("Key: {}, Value: {:?}", key, value);
     }
     
-    println!("Creating a vector from the config");
-    let mut primary_layer_buttons = Vec::new();
-    for button_config in &config.layers.primary_layer_buttons.buttons {
-        let label = button_config.label.as_str();
-        let key = &button_config.key.as_str();
-        match key_map.get(key) {
-            Some(value) => {
-                primary_layer_buttons.push(Button::new_text(label, *value));
-                println!("Found key {}: with value {:?}", label, value );
-            }
-            None => println!("Could not find {key} in the map.")
-        }
+    let mut layers: Vec<Button> = initialize_layers();
+    for button in layers {
+        println!("looping: key {}: with value {:?}", button.label, button.action );
     }
-}
+    
 }
